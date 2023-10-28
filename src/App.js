820c6343221600ce77c384be9f6cab65f71ddf3c
Mahmoud Mostafa <mahmoud93.mostafa93@gmail.com>
2023-10-28 20:48:58 +0200

    Finish FriendsList and Friend components

--- src/App.js
+++ src/App.js
@@ -20,5 +20,55 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Hello Mahmoud!</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        <button className="button">Add friend</button>
+      </div>
+    </div>
+  );
 }
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li key={friend.id}>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}€
+        </p>
+      )}
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {friend.balance * -1}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
+}
+
+/* Component tree 
+------------------
+                App
+      /         |             \
+FriendsList   AddFriend     SplitBill
+      |          
+    Friend       
+*/
